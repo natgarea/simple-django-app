e54293a5e6dd72465185dfa734d4caeaf4ae7010
Natalia Garea <natgarea@gmail.com>
2020-08-11 18:07:05 +0200

    Add a template

--- beers/views.py
+++ beers/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 def first_view(request):
-    return HttpResponse("Saludos")
+    # return HttpResponse("Saludos")
+
+    context = {
+        'sample_var': "ejemplo"
+    }
+
+    return render(request, 'beers.html', context)
